f91d5497ae3d149688a86554a940223822085281
Wongani Chulu <wonganichulu@gmail.com>
2024-09-30 14:56:39 +0200

    Added 1-block-scoped.js

---
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -0,0 +1,14 @@
+export default function taskBlock(trueOrFalse) {
+  var task = false;
+  var task2 = true;
+
+  if (trueOrFalse) {
+    var task = true;
+    var task2 = false;
+  }
+
+  task = false;
+  task2 = true;
+
+  return [task, task2];
+}
