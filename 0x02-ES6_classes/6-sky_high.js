b32f8013e65c48b46ea3464187a2daba19c0b997
Wongani Chulu <wonganichulu@gmail.com>
2024-10-03 22:28:00 +0200

    Added 6-sky_high.js fix

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -7,6 +7,10 @@ export default class SkyHighBuilding extends Building {
     this._floors = floors;
   }
 
+  set floors(floors) {
+    this._floors = floors;
+  }
+
   get floors() {
     return this._floors;
   }
