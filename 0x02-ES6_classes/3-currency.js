ba5c4d0c098b9405140300516a847c2e36ae093a
Wongani Chulu <wonganichulu@gmail.com>
2024-10-03 21:47:57 +0200

    Added 3-currency.js

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,33 @@
+/* eslint-disable no-underscore-dangle */
+export default class Currency {
+  constructor(code, name) {
+    this.code = code;
+    this.name = name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set code(code) {
+    if (typeof code !== 'string') {
+      throw new TypeError('Code must be a string');
+    }
+    this._code = code;
+  }
+
+  set name(name) {
+    if (typeof name !== 'string') {
+      throw new TypeError('Code must be a string');
+    }
+    this._name = name;
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}
