e83df5d4d2c6811f3cbafcb357b557a9807587b1
Wongani Chulu <wonganichulu@gmail.com>
2024-10-10 03:46:54 +0200

    Added task_2/js/main.ts fix

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -1,18 +1,9 @@
-interface Teacher {
-  readonly firstName: string;
-  readonly lastName: string;
-  fullTimeEmployee: boolean;
-  yearsOfExperience?: number;
-  location: string;
-  [key: string]: any;
+interface PrintTeacherFunction {
+  (firstName: string, lastName: string): string;
 }
 
-const teacher3: Teacher = {
-  firstName: 'John',
-  lastName: 'Doe',
-  fullTimeEmployee: false,
-  location: 'London',
-  contract: false,
+const printTeacher: PrintTeacherFunction = (firstName: string, lastName: string): string => {
+  return `${firstName.charAt(0)}. ${lastName}`;
 };
 
-console.log(teacher3);
+console.log(printTeacher("John", "Doe"));
