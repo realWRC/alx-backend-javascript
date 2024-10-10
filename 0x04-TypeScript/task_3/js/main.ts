0c164adf12d2cf1569d68cce9d166c4b35fb8882
Wongani Chulu <wonganichulu@gmail.com>
2024-10-10 03:51:31 +0200

    Added task_3

---
+++ 0x04-TypeScript/task_3/js/main.ts
@@ -0,0 +1,32 @@
+interface StudentConstructor {
+  firstName: string;
+  lastName: string;
+}
+
+interface StudentClassInterface {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+class StudentClass implements StudentClassInterface {
+  firstName: string;
+  lastName: string;
+
+  constructor({ firstName, lastName }: StudentConstructor) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return 'Currently working';
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
+
+const student = new StudentClass({ firstName: 'John', lastName: 'Doe' });
+console.log(student.displayName());
+console.log(student.workOnHomework());
+
