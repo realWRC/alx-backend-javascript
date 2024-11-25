3daf8fe6d65deb3841433e41e2c42942b45d836a
Wongani Chulu <wonganichulu@gmail.com>
2024-11-25 18:09:24 +0200

    Added 1-stdin.js fix

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,21 +1,5 @@
 // Displays a message with a newline at the
 
-// console.log('Welcome to Holberton School, what is your name?');
-//
-// process.stdin.setEncoding('utf8');
-//
-// process.stdin.on('data', (input) => {
-//   const name = input.trim();
-//   console.log(`Your name is: ${name}`);
-//   if (process.stdin.isTTY) {
-//     process.exit(0);
-//   }
-// });
-//
-// process.stdin.on('end', () => {
-//   console.log('This important software is now closing');
-// });
-
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 if (process.stdin.isTTY) {
