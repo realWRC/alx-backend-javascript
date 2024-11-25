553c34ac738df4346e0fd1ba430e12c3f75daa96
Wongani Chulu <wonganichulu@gmail.com>
2024-11-25 18:06:08 +0200

    Added 1-stdin.js fix

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,17 +1,34 @@
 // Displays a message with a newline at the
 
-console.log('Welcome to Holberton School, what is your name?');
+// console.log('Welcome to Holberton School, what is your name?');
+//
+// process.stdin.setEncoding('utf8');
+//
+// process.stdin.on('data', (input) => {
+//   const name = input.trim();
+//   console.log(`Your name is: ${name}`);
+//   if (process.stdin.isTTY) {
+//     process.exit(0);
+//   }
+// });
+//
+// process.stdin.on('end', () => {
+//   console.log('This important software is now closing');
+// });
 
-process.stdin.setEncoding('utf8');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.on('data', (input) => {
-  const name = input.trim();
-  console.log(`Your name is: ${name}`);
-  if (process.stdin.isTTY) {
-    process.exit(0);
-  }
-});
-
-process.stdin.on('end', () => {
-  console.log('This important software is now closing');
-});
+if (process.stdin.isTTY) {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.exit();
+  });
+} else {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.exit();
+  });
+  process.on('exit', () => {
+    process.stdout.write('This important software is now closing\n');
+  });
+}
