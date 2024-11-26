49686c627bc494f18c0d89dd5bb578751a41d13a
Wongani Chulu <wonganichulu@gmail.com>
2024-11-26 16:26:02 +0200

    Added 4-http.js

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,16 @@
+// A simple HTTP server in node
+
+const http = require("http");
+
+const host = 'localhost';
+const port = 1245;
+
+const app = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello Holberton School!');
+});
+
+app.listen(port, host, () => {});
+
+module.exports = app;
