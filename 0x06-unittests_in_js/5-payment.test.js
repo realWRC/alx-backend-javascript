534288700cc702601fea74aa238ac2d2ab137ddf
Wongani Chulu <wonganichulu@gmail.com>
2024-11-28 19:54:12 +0200

    Added 5-payment.js and 5-payment.test.js

---
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -0,0 +1,32 @@
+// Test suit for 5-payment.js 
+
+const sinon = require('sinon');
+const { expect } = require('chai');
+
+const sendPaymentRequestToApi = require('./5-payment');
+
+describe('sendPaymentRequestToApi', function () {
+  let consoleSpy;
+
+  beforeEach(function () {
+    consoleSpy = sinon.spy(console, 'log');
+  });
+
+  afterEach(function () {
+    consoleSpy.restore();
+  });
+
+  it('should log the correct total for 100 and 20', function () {
+    sendPaymentRequestToApi(100, 20);
+
+    expect(consoleSpy.calledOnceWithExactly('The total is: 120')).to.be.true;
+    expect(consoleSpy.calledOnce).to.be.true;
+  });
+
+  it('should log the correct total for 10 and 10', function () {
+    sendPaymentRequestToApi(10, 10);
+
+    expect(consoleSpy.calledOnceWithExactly('The total is: 20')).to.be.true;
+    expect(consoleSpy.calledOnce).to.be.true;
+  });
+});
